622fb14d289cbbb8a31a0916b31b66f7fd56cd78
Madison Sevcik <madisonsevcik@gmail.com>
2026-07-24 13:21:08 -0400

    ms

--- HVAC-controller/components/heater_manager.c
+++
@@ -1,2 +0,0 @@
-#include "heater_manager.h"
-
--- HVAC-controller/components/sensor_manager.c
+++
@@ -1,7 +0,0 @@
-#include "sensor_manager.h"
-#include "esp_adc/adc_oneshot.h"
-#include "esp_timer.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-
-
