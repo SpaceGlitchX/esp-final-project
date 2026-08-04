1d2079d80fff4f4917e1371a253564fa1f12c025
Spoopidy <Prof.sp00k@gmail.com>
2026-08-04 00:53:27 -0400

    mapped hardware pins to variable names according to schematic I made. Also I broke out everything into a new header file (and updated the Cmake list to recognize it)

--- smart-thermostat/main/main.c
+++ smart-thermostat/main/main.c
@@ -1,41 +1,4 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-
-/*
-    THE SCHEME:
-    ESPs receive and output data to peripherals via GPIO
-    2 ESPs communicate via UART using byte-array
-
-    BAUD RATE: 
-    TICK RATE:  10 seconds 
-
-    HVAC:
-        waits for input from thermostat and sensor inputs
-        
-        INPUTS:
-        pilot light sensor
-        fan speed sensor
-        
-        furnace on/off command from thermostat
-
-        OUTPUTS:
-        system status: fan on and heat on flags
-        error flags
-
-    THERMOSTAT:
-
-        INPUTS:
-        temperature
-        humidity
-
-        user-determined tempertaure target point
-
-        OUTPUTS:
-        UART to HVAC (furnace on/off command)
-        LCD or LED indicators
-*/
+#include "thermostat.h"
 
 void app_main(void)
 {
