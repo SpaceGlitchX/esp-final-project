176d3f472c1c7ef7fb25d16f063494c3dfa5eaa1
Madison Sevcik <madisonsevcik@gmail.com>
2026-08-10 17:01:58 -0400

    ms

---
+++ smart-thermostat/main/ui.c
@@ -0,0 +1,8 @@
+#include "ui.h"
+
+void ui_init(void) {
+
+    i2c_lcd_init();
+
+    
+}
--- smart-thermostat/main/ui.h
+++ smart-thermostat/main/ui.h
@@ -1,5 +1,24 @@
-#ifndef LCD_H
-#define LCD_H
+#ifndef UI_H
+#define UI_H
 
 #include "LCD.h"
-#include
+
+#define SET_UP GPIO_NUM_4
+#define SET_DWN GPIO_NUM_0
+#define SEL_UP GPIO_NUM_2
+#define SEL_DWN GPIO_NUM_15
+
+typedef struct {
+    void* setting;
+    void* data;
+    char* top_text;
+    char* bottom_text;
+} view;
+
+typedef struct {
+    
+}
+extern void ui_init(void);
+
+
+#endif
